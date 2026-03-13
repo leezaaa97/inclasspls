883479366e720d6bed1fde9d5bd267b26c7b5694
leezaaa97 <laleezagilbert@gmail.com>
2026-03-13 20:35:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-//part config 
 const port = 3001;
-//express config
 const exp = require('express');
-//config the server application
 const app = exp();
 
 app.use(exp.json());
+app.use(exp.urlencoded({ extended: true })); 
 app.use(exp.static('public'));
 
+let storedName = "";
 
-let name = "";
-let batch = "";
-
-app.get('/api',(req,res)=>{
-    res.json({message:"HELLO"})
+app.post('/submit-name', (req, res) => {
+    storedName = req.body.username; 
+    res.redirect('/greet');
 });
 
-app.listen(port,()=>{
-    console.log(`server started at port: ${port}`);
+app.get('/greet', (req, res) => {
+    res.send(`
+        <div style="text-align: center; margin-top: 50px;">
+            <h1>Hello, ${storedName}!</h1>
+            <br>
+            <a href="/">Go Back</a>
+        </div>
+    `);
 });
 
+app.get('/api', (req, res) => {
+    res.json({ message: "HELLO" });
+});
 
-//app.listen(3001);
+app.listen(port, () => {
+    console.log(`Server started at port: ${port}`);
+});
